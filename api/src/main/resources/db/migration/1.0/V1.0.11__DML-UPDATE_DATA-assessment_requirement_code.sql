a701c0e0c408c2bea42757d2b758fbbf442880a4
Jinil Sung <infstar@gmail.com>
2022-11-28 16:43:17 -0800

    Create V1.0.11__DML-UPDATE_DATA-assessment_requirement_code.sql
    GRAD2-1637: the task is completed

---
+++ api/src/main/resources/db/migration/1.0/V1.0.11__DML-UPDATE_DATA-assessment_requirement_code.sql
@@ -0,0 +1,8 @@
+UPDATE ASSESSMENT_REQUIREMENT_CODE SET LABEL = 'Évaluation de littératie 12 – immersion', DESCRIPTION = 'Évaluation de littératie 12 – immersion'
+WHERE ASSESSMENT_REQUIREMENT_CODE = '203';
+
+UPDATE ASSESSMENT_REQUIREMENT_CODE SET LABEL = 'Évaluation de littératie 10 – langue première', DESCRIPTION = 'Évaluation de littératie 10 – langue première'
+WHERE ASSESSMENT_REQUIREMENT_CODE = '303';
+
+UPDATE ASSESSMENT_REQUIREMENT_CODE SET LABEL = 'Évaluation de littératie 12 – langue première', DESCRIPTION = 'Évaluation de littératie 12 – langue première'
+WHERE ASSESSMENT_REQUIREMENT_CODE = '304';
