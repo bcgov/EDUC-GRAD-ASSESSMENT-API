1cdddd63d00806b045bbeb948cb3f23513f05f30
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-03-08 11:42:10 -0700

    Update actions

--- tools/config/override-configmap-dev.sh
+++ tools/config/override-configmap-dev.sh
@@ -3,9 +3,11 @@
 ###########################################################
 envValue=$1
 APP_NAME=$2
-OPENSHIFT_NAMESPACE=$3
-SPLUNK_TOKEN=$4
-APP_LOG_LEVEL=$5
+GRAD_NAMESPACE=$3
+COMMON_NAMESPACE=$4
+BUSINESS_NAMESPACE=$5
+SPLUNK_TOKEN=$6
+APP_LOG_LEVEL=$7
 
 SPLUNK_URL="gww.splunk.educ.gov.bc.ca"
 FLB_CONFIG="[SERVICE]
@@ -39,8 +41,7 @@ PARSER_CONFIG="
 #Override config-maps in DEV
 ###########################################################
 echo Creating config map "$APP_NAME"-flb-sc-config-map
-oc create -n "$OPENSHIFT_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
+oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-map \
   --from-literal=fluent-bit.conf="$FLB_CONFIG" \
   --from-literal=parsers.conf="$PARSER_CONFIG" \
   --dry-run=client -o yaml | oc apply -f -
-
