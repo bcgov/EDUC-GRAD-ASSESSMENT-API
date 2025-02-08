31792f6f07f3764302614940ec9c1e21a80e1406
Jinil Sung <infstar@gmail.com>
2025-02-07 16:11:19 -0800

    GRAD2-3253: the issue is resolved. (#291)
    * GRAD2-3253: the issue is resolved.
    
    GRAD2-3253: the issue is resolved.
    
    * Fixed the broken unit tests.
    
    Fixed the broken unit tests.

--- api/src/main/java/ca/bc/gov/educ/api/assessment/service/StudentAssessmentService.java
+++ api/src/main/java/ca/bc/gov/educ/api/assessment/service/StudentAssessmentService.java
@@ -1,20 +1,15 @@
 package ca.bc.gov.educ.api.assessment.service;
 
-
 import ca.bc.gov.educ.api.assessment.model.dto.Assessment;
-import ca.bc.gov.educ.api.assessment.model.dto.School;
 import ca.bc.gov.educ.api.assessment.model.dto.StudentAssessment;
 import ca.bc.gov.educ.api.assessment.model.transformer.AssessmentTransformer;
 import ca.bc.gov.educ.api.assessment.model.transformer.StudentAssessmentTransformer;
 import ca.bc.gov.educ.api.assessment.repository.AssessmentRepository;
 import ca.bc.gov.educ.api.assessment.repository.StudentAssessmentRepository;
-import ca.bc.gov.educ.api.assessment.util.EducAssessmentApiConstants;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -37,12 +32,6 @@ public class StudentAssessmentService {
 
     @Autowired
     AssessmentTransformer assessmentTransformer;
-
-    @Autowired
-    WebClient webClient;
-
-    @Autowired
-    EducAssessmentApiConstants constants;
 
     /**
      * Get all student assessments by PEN populated in Student Assessment DTO
@@ -90,18 +79,6 @@ public class StudentAssessmentService {
             if (assessment != null) {
                 sA.setAssessmentName(assessment.getAssessmentName());
                 sA.setAssessmentDetails(assessment);
-            }
-
-            if (StringUtils.isNotBlank(sA.getMincodeAssessment())) {
-                School schObj = webClient.get()
-                        .uri(String.format(constants.getSchoolNameByMincodeUrl(), sA.getMincodeAssessment()))
-                        .headers(h -> h.setBearerAuth(accessToken))
-                        .retrieve()
-                        .bodyToMono(School.class)
-                        .block();
-                if (schObj != null)
-                    sA.setMincodeAssessmentName(schObj.getDisplayName());
-                sA.setHasMoreInfo(true);
             }
         });
     }
